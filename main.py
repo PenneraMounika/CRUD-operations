432a241df3dbdff808c7968096fdc45d33b7acba
PenneraMounika <penneramounika@gmail.com>
2024-07-02 16:18:56 +0530

    Deletes the Specific row from the student table

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ cur.execute(
     """CREATE TABLE IF NOT EXISTS teacher(employee_id integer primary key autoincrement,name varchar(250) not null,age int not null,doj date not null);"""
 )
 
-count = 0
-
 
 def insert_student_details(student_name, student_dob):
     query = "INSERT INTO student(name,dob)VALUES (?,?)"
@@ -54,6 +52,17 @@ def update_teacher_doj(new_data, tr_id):
     cur.execute(query, (new_data, tr_id))
     print("Teacher doj updated successfully.")
 
+def display_student_row_to_delete(row_selection):
+    query="SELECT * FROM STUDENT WHERE id=?;"
+    for row in cur.execute(query,(row_selection,)):
+        print(row)
+    else:
+        print("Row does not exist.")
+        return False
+
+def delete_student_row(row_selection):
+    query="DELETE FROM STUDENT WHERE id=?"
+    cur.execute(query,(row_selection,))
 
 user_input = int(
     input("SELECT OPERATION TO BE PERFORMED:\n1.CREATE\n2.READ\n3.UPDATE\n4.DELETE\n")
@@ -74,11 +83,14 @@ while user_input <= 4 and user_input > 0:
             teacher_doj = input("Enter date of joining:")
             insert_teacher_details(teacher_name, teacher_age, teacher_doj)
         else:
+            print("No such table exists.")
             break
+        conn.commit()
 
     elif user_input == 2:
         table_input = int(input("CHOOSE TABLE\n1.STUDENT\n2.TEACHER\n"))
         if table_input == 1:
+            count=0
             query = "SELECT * FROM STUDENT;"
             for row in cur.execute(query):
                 count += 1
@@ -99,6 +111,7 @@ while user_input <= 4 and user_input > 0:
                     print(row)
                 print("Teacher table displayed successfully.\n")
         else:
+            print("No such table exists.")
             break
 
     elif user_input == 3:
@@ -115,6 +128,7 @@ while user_input <= 4 and user_input > 0:
                 new_data = input("Enter student dob to be updated: ")
                 update_student_dob(new_data, st_id)
             else:
+                print("Column should be either 1 or 2.")
                 break
         elif table_input == 2:
             tr_id = int(
@@ -135,8 +149,34 @@ while user_input <= 4 and user_input > 0:
                 new_data = input("Enter teacher doj to be updated:")
                 update_teacher_doj(new_data, tr_id)
             else:
+                print("Column should be either 1,2 or 3.")
                 break
+        else:
+            print("No such table exists.")
+            break
+        conn.commit()
 
+    elif user_input==4:
+        table_input=int(input("CHOOSE TABLE:\n1.STUDENT\n2.TEACHER\n"))
+        if table_input==1:
+            query = "SELECT * FROM STUDENT;"
+            for row in cur.execute(query):
+                count += 1
+            if count == 0:
+                print("No rows to delete in student table.")
+            else:
+                row_selection=int(input("Enter the id to remove that row from the table:"))
+                row_existence=display_student_row_to_delete(row_selection)
+                if row_existence!=False:
+                    confirm_user_input=input("Confirm deletion of the row selected above:")
+                    if confirm_user_input=="yes":
+                        delete_student_row(row_selection)
+                        print("Selected row deleted successfully")
+                    else:
+                        print("Deletion cancelled")
+                        pass
+        conn.commit()
+    
     user_input = int(
         input(
             "SELECT OPERATION TO BE PERFORMED:\n1.CREATE\n2.READ\n3.UPDATE\n4.DELETE\n"
